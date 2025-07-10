766b8b5ed4f3b628c450a703471bec968bfd31c6
Joanna Huang <70039418+jch486@users.noreply.github.com>
2025-07-10 09:55:07 -0500

    constructed outcomes

---
+++ construct_outcomes.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import os
+import numpy as np
+import math
+
+def construct_outcomes(features_vectors, tb_dx_visits_df):
+    outcomes = pd.DataFrame()
+
+    # for each patient (each 30-day window before a visit is a patient)
+    for features in features_vectors.itertuples(index=False):
+        patient_id = features[0]
+        date = features[1]
+        # find whether or not the patient was diagnosed with TB on their visit
+        has_tb = ((tb_dx_visits_df['patient_id'] == patient_id) & (tb_dx_visits_df['date'] == date)).any()
+        # add outcomes for that visit to dataframe
+        new_row = pd.DataFrame({'example_id': [[patient_id, date]], 'has_tb': [int(has_tb)], 'has_no_tb': [int(not has_tb)]})
+        outcomes = pd.concat([outcomes, new_row], ignore_index=True)
+    
+    return outcomes
+
+def main():
+    # set up file paths and load data
+    features_vectors_fn = os.path.join('other_data', 'features_vectors.csv')
+    features_vectors = pd.read_csv(features_vectors_fn)
+    tb_dx_visits_fn = os.path.join('subsample', 'tb_dx_visits.csv')
+    tb_dx_visits_df = pd.read_csv(tb_dx_visits_fn)
+    outcomes_fn = os.path.join('other_data', 'outcomes.csv')
+
+    if not os.path.exists(outcomes_fn):
+        outcomes = construct_outcomes(features_vectors, tb_dx_visits_df)
+        outcomes.to_csv(outcomes_fn, index=False)
+
+    outcomes = pd.read_csv(outcomes_fn)
+
+if __name__ == '__main__':
+    main()
