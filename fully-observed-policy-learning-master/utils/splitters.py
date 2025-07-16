b87031f397d1d277b7d8bb0466ac08f983166bc4
Joanna Huang <70039418+jch486@users.noreply.github.com>
2025-07-16 00:57:09 -0500

    splitting each class evenly

--- fully-observed-policy-learning-master/utils/splitters.py
+++ fully-observed-policy-learning-master/utils/splitters.py
@@ -1,20 +1,43 @@
 import logging
 from sklearn.utils import shuffle
+import pandas as pd
 
-# shuffles unique elements of the cohort and then splits it in two based on train_size
-def split_pids(cohort_info_df,
+# split cohort into each outcome, then shuffles unique elements of the cohort 
+# and splits it in two based on train_size
+def split_pids(cohort_info_df, outcomes_df,
                 seed, train_size=.7): 
 
+    cohort_df = cohort_info_df.merge(outcomes_df, on='example_id')
+    has_tb = cohort_df[cohort_df['has_tb'] == 1]
+    has_no_tb = cohort_df[cohort_df['has_no_tb'] == 1]
+
+    pids_tb = sorted(has_tb['person_id'].unique())
+    shuffled_pids_tb = shuffle(pids_tb, random_state=seed)
+    cutoff = int(len(shuffled_pids_tb)*train_size)
+    train_pids_tb, val_pids_tb = shuffled_pids_tb[:cutoff], shuffled_pids_tb[cutoff:]
+
+    pids_no_tb = sorted(has_no_tb['person_id'].unique())
+    shuffled_pids_no_tb = shuffle(pids_no_tb, random_state=seed)
+    cutoff = int(len(shuffled_pids_no_tb)*train_size)
+    train_pids_no_tb, val_pids_no_tb = shuffled_pids_no_tb[:cutoff], shuffled_pids_no_tb[cutoff:]
+
+    logging.info(f"Train TB proportion: {len(train_pids_tb) / (len(train_pids_tb) + len(train_pids_no_tb))}")
+    logging.info(f"Val TB proportion: {len(val_pids_tb) / (len(val_pids_tb) + len(val_pids_no_tb))}")
+    
+    return train_pids_tb + train_pids_no_tb, val_pids_tb + val_pids_no_tb
+
+    '''
     # unique() returns unique values
     pids = sorted(cohort_info_df['person_id'].unique())
     shuffled_pids = shuffle(pids, random_state=seed)
     cutoff = int(len(shuffled_pids)*train_size)
     train_pids, val_pids = shuffled_pids[:cutoff], shuffled_pids[cutoff:]
-    
+
     return train_pids, val_pids
+    '''
 
 
-
+# cohort_info_df = metadata
 def split_cohort_abx(cohort_df, outcomes_df, 
                      cohort_info_df,
                      seed, train_size=.7):
@@ -26,7 +49,7 @@ def split_cohort_abx(cohort_df, outcomes_df,
         and validation sets.
     '''
 
-    train_pids, val_pids = split_pids(cohort_info_df, seed, train_size)
+    train_pids, val_pids = split_pids(cohort_info_df, outcomes_df, seed, train_size)
 
     train_eids = cohort_info_df[cohort_info_df['person_id'].isin(set(train_pids))]['example_id'].values
     val_eids = cohort_info_df[cohort_info_df['person_id'].isin(set(val_pids))]['example_id'].values
